122c9fa6858b904aa5a1a2afaff1dc989d7605eb
DunchaLine <voskk1999@gmail.com>
2020-02-18 16:26:31 +0300

    небольшие правки

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -7,15 +7,12 @@ public class BallMovement : MonoBehaviour
     public float _radius;
     [SerializeField]
     private float _angle;
-    //public float _speed;
     public bool clockwise;
     public GameObject _center;
-    // private Rigidbody2D _rb;
     void Start()
     {
         clockwise = true;
         _angle = -20;
-        //_rb = GetComponent<Rigidbody2D>();
         Time.timeScale = 2f;
     }
 
@@ -28,17 +25,8 @@ public class BallMovement : MonoBehaviour
         else
         if (clockwise)
         {
-            //var x = _center.transform.position.x + _radius * Mathf.Sin(_angle) * _speed;
-            //var y = _center.transform.position.y + _radius * Mathf.Cos(_angle) * _speed;
             _angle += Time.deltaTime;
             BallMove(_angle);
-            //float step = _speed / Time.deltaTime;
-            //transform.position = Vector3.MoveTowards(transform.position, new Vector3(x, y), step);
-            // Vector3 main = new Vector3(x, y);
-            // Debug.Log("main: " + main);
-            // _rb.velocity = main;
-            // Vector3 _dir = new Vector3(x, y) - _currPos;
-            // transform.Translate(_dir * Time.deltaTime * _speed);
         }
         if (!clockwise)
         {
