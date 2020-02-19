03e37a44c366a7b15b881ebb279c375087dfe73d
DunchaLine <voskk1999@gmail.com>
2020-02-19 18:34:04 +0300

    Добавил полоску, которая в будущем будет менять цвет при соприкосновении с шаром

--- Assets/Scripts/SpawnObj.cs
+++ Assets/Scripts/SpawnObj.cs
@@ -5,26 +5,51 @@ using UnityEngine;
 public class SpawnObj : MonoBehaviour
 {
     public GameObject[] objToSpawn;
+    public GameObject managerObj;
+    private gManager manager;
     private IEnumerator coroutine;
     private GameObject tmp;
     void Start()
     {
-        coroutine = InstantiateObj(Random.Range(3f, 4f));
+        manager = managerObj.GetComponent<gManager>();
+        coroutine = InstantiateObj(Random.Range(3f, 4f), manager.Line);
         StartCoroutine(coroutine);
     }
 
     void Update()
     {
         
+        if (manager.Line == true)
+        {
+            Debug.Log("Stop it now");
+            StopAllCoroutines();
+            coroutine = InstantiateObj(Random.Range(0f, 0f), true);
+            StartCoroutine(coroutine);
+            if (GameObject.FindGameObjectWithTag("Line") == null)
+            {
+                Debug.Log("Now u can continue");
+                StopAllCoroutines();
+                manager.Line = false;
+                coroutine = InstantiateObj(Random.Range(3f, 4f), false);
+                StartCoroutine(coroutine);
+            }
+        }
+            
     }
 
-    private IEnumerator InstantiateObj(float _time)
+    private IEnumerator InstantiateObj(float _time, bool _line)
     {
-        while (true)
+        while (_line == false)
         {
+            Debug.Log("Or maybe dont stop?");
             yield return new WaitForSeconds(_time);
             tmp = Instantiate(objToSpawn[Random.Range(0, objToSpawn.Length)], transform.position, Quaternion.identity) as GameObject;
             Destroy(tmp, 10f);
+        }
+        while (_line == true)
+        {
+            yield return null;
+            Debug.Log("It must stop right now");
         }
     }
 }
---
+++ Assets/Scripts/gManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool lineSpawned = true;
+    void Start()
+    {
+        Line = false;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public bool Line
+    {
+        get
+        {
+            return lineSpawned;
+        }
+        set
+        {
+            lineSpawned = value;
+        }
+    }
+}
